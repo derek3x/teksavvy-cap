48b9920ada32840df13722ee6ba093cc059c814b
derek3x <derek3x@vmail.me>
2014-01-20 10:38:04 -0500

    Update main.py
    Cleaned up code a bit

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
+#!/usr/bin/env python
+
 import spynner
 from bs4 import BeautifulSoup
 from getpass import getpass
 
-username = raw_input('Enter your email address: ')
-password = getpass('Enter your password: ')
-b = spynner.Browser()
-b.load('https://myaccount.teksavvy.com/')
-b.wk_fill('input[name=login]', username)
-b.wk_fill('input[name=password]', password)
-b.click(".tsi-full-width > button:nth-child(1)")
-b.click(".table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(4) > a:nth-child(1)")
-b.wait(3)
-b.click('a[href="/Services/ServiceDetails/466259"]')
-b.wait(3)
-b.click('a[href="/Services/ServiceDetails/466259/ViewUsage]')
-soup = BeautifulSoup(b.html)
+def main():
+     username = raw_input('Enter your email address: ')
+     password = getpass('Enter your password: ')
+     print
+     print 'Grabbing info........'
+     print
+     b = spynner.Browser()
+     b.load('https://myaccount.teksavvy.com/')
+     b.wk_fill('input[name=login]', username)
+     b.wk_fill('input[name=password]', password)
+     b.click(".tsi-full-width > button:nth-child(1)")
+     b.click(".table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(4) > a:nth-child(1)")
+     b.wait(3)
+     b.click('a[href="/Services/ServiceDetails/466259"]')
+     b.wait(3)
+     b.click('a[href="/Services/ServiceDetails/466259/ViewUsage]')
+     soup = BeautifulSoup(b.html)
 
-usage = soup.find('span', {'class' : 'tsi-monthlyUsageLabel'})
-print 'Used: ' + usage.encode_contents()
-remaining = soup.find('div', {'class' : 'tsi-progresLabelBottom'})
-print remaining.encode_contents()
-cap = soup.find('span', {'class' : 'tsi-progresLabelRigth'})
-print 'Cap: ' + cap.encode_contents()
+     usage = soup.find('span', {'class' : 'tsi-monthlyUsageLabel'})
+     print 'Used: ' + usage.encode_contents()
+     remaining = soup.find('div', {'class' : 'tsi-progresLabelBottom'})
+     print remaining.encode_contents()
+     cap = soup.find('span', {'class' : 'tsi-progresLabelRigth'})
+     print 'Cap: ' + cap.encode_contents()
+
+if __name__ == '__main__':
+     main()
